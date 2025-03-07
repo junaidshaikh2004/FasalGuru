eecf813d69889e6a58173bea2f24df0b94464c20
Junaid Shaikh <junaidshaikh20004@gmail.com>
2025-03-07 16:59:54 +0530

    cards

--- frontend/src/pages/Features.tsx
+++ frontend/src/pages/Features.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
 import '../styles/features.css'
 import { IoIosArrowDropright } from "react-icons/io";
-import { Navigate, useNavigate } from 'react-router-dom';
 
 function Features() {
-  const navigate = useNavigate();  // This line is required
   return (
     <div  className='feature '>
       <div className='flex items-center justify-center gap-2'>
@@ -16,15 +14,46 @@ function Features() {
         <img src="src/assets/pest2.jpg" alt="" className='f_image' />
         <h1 className='text-3xl font-bold mt-2' >Pest Detection and Prevention</h1>
         <p>Snap or upload a photo to identify pests. Our system will analyze it and suggest the pest's name and solutions.</p> 
-      <button onClick={() => navigate('/pest-detection')}  className="mt-4 flex justify-between w-50 h-10 items-center rounded-2xl px-3 bg-green-600">
-      <h1 className='text-start  text-xl font-bold text-white ' >Use Now</h1>
+      <div className="mt-4 flex justify-between w-50 h-10 items-center rounded-2xl px-3 bg-green-600">
+      <button className='text-start  text-xl font-bold text-white ' >Use Now</button>
       <div className='w-8 h-8  text-white '>
       <IoIosArrowDropright className='w-[100%] h-[100%] color-whiy ' />
       </div>
-      </button>
       </div>
-      <div className="f_card"></div>
-      <div className="f_card"></div>
+      </div>
+      <div className="f_card">
+        <img src="src/assets/pest2.jpg" alt="" className='f_image' />
+        <h1 className='text-3xl font-bold mt-2' >Whole Sale Price Index Prediction</h1>
+        <p>AI-powered WPI Prediction on our website forecasts future wholesale prices using historical data, helping users make smarter pricing decisions.</p> 
+      <div className="mt-4 flex justify-between w-50 h-10 items-center rounded-2xl px-3 bg-green-600">
+      <button className='text-start  text-xl font-bold text-white ' >Use Now</button>
+      <div className='w-8 h-8  text-white '>
+      <IoIosArrowDropright className='w-[100%] h-[100%] color-whiy ' />
+      </div>
+      </div>
+      </div>
+      <div className="f_card">
+        <img src="src/assets/pest2.jpg" alt="" className='f_image' />
+        <h1 className='text-3xl font-bold mt-2' >Crop Recommendation</h1>
+        <p>Snap or upload a photo to identify pests. Our system will analyze it and suggest the pest's name and solutions.</p> 
+      <div className="mt-4 flex justify-between w-50 h-10 items-center rounded-2xl px-3 bg-green-600">
+      <button className='text-start  text-xl font-bold text-white ' >Use Now</button>
+      <div className='w-8 h-8  text-white '>
+      <IoIosArrowDropright className='w-[100%] h-[100%] color-whiy ' />
+      </div>
+      </div>
+      </div>
+      <div className="f_card">
+        <img src="src/assets/pest2.jpg" alt="" className='f_image' />
+        <h1 className='text-3xl font-bold mt-2' >Fertilizer Recommendation</h1>
+        <p>Snap or upload a photo to identify pests. Our system will analyze it and suggest the pest's name and solutions.</p> 
+      <div className="mt-4 flex justify-between w-50 h-10 items-center rounded-2xl px-3 bg-green-600">
+      <button className='text-start  text-xl font-bold text-white ' >Use Now</button>
+      <div className='w-8 h-8  text-white '>
+      <IoIosArrowDropright className='w-[100%] h-[100%] color-whiy ' />
+      </div>
+      </div>
+      </div>
       </div>
     </div>
   )
